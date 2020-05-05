541526c11175ccc9f4d85b8640e8a0873682c93e
parag khodke <paragkhodke72@gmail.com>
2020-05-04 20:13:44 -0400

    Frogs

---
+++ frogs-and-toads/src/main/java/com/mycompany/frogsandtoads/InstructionsController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.frogsandtoads;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author paragkhodke
+ */
+public class InstructionsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void BACK(ActionEvent event) throws IOException {
+        App.setRoot("welcome");
+    }
+
+}
---
+++ frogs-and-toads/src/main/java/com/mycompany/frogsandtoads/SecondaryController.java
@@ -0,0 +1,16 @@
+package com.mycompany.frogsandtoads;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SecondaryController {
+
+    @FXML
+    private Button secondaryButton;
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
